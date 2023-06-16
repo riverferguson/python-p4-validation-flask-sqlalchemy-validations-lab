76969a1ca1bacc7a42b8b624ef302ce0dcec092d
River Ferguson <riverferguson@gmail.com>
2023-06-15 22:19:08 -0700

    complete lab

--- server/models.py
+++ server/models.py
@@ -14,6 +14,21 @@ class Author(db.Model):
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
+    
+    @validates('name')
+    def validate_name(self, key, name):
+        names = db.session.query(Author.name).all()
+        if not name:
+            raise ValueError
+        elif name in names:
+            raise ValueError
+        return name
+    
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if len(phone_number) != 10:
+            raise ValueError
+        return phone_number
 
 class Post(db.Model):
     __tablename__ = 'posts'
@@ -26,6 +41,29 @@ class Post(db.Model):
     summary = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    
+    @validates('content', 'summary')
+    def validate_length(self, key, string):
+        if(key == 'content'):
+            if len(string) <= 250:
+                raise ValueError
+        if(key == 'summary'):
+            if len(string) >= 250:
+                raise ValueError
+            return string
+        
+    @validates('title')
+    def validate_title(self, key, title):
+        clkbt = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(string in title for string in clkbt):
+            raise ValueError
+        return title
+    
+    @validates('category')
+    def validate_category(self, key, category):
+        if category != 'Fiction' and category != 'Non-Fiction':
+            raise ValueError
+        return category          
 
 
     def __repr__(self):
